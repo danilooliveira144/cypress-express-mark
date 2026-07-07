211199627625779caa9317e3064f8735e8148a6b
Danilo Oliveira <danilooliveira4586@gmail.com>
2026-07-07 16:45:12 -0300

    Adicionando a função contains do cypress para localizar o botão através do nome

--- cypress/e2e/task.cy.js
+++ cypress/e2e/task.cy.js
@@ -3,9 +3,14 @@
 describe('Tarefas', ()=>{
 
     it('Deve realizar o cadastro da tarefa com sucesso', ()=>{
+        //acessando o endereço do sistema
         cy.visit('http://localhost:3000/')
 
+        // Adicionando um valor ao campo de texto atráves do atributo placeholder do elemento input
         cy.get('input[placeholder="Add a new task"]')
             .type('Estudando cypress')
+        
+        // Clicando no botão de criar, utilizando a função contains do cypress
+        cy.contains('button', 'Create').click()
     })
 })
