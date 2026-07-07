328022086e406e31b495b1f565274d2867f67be0
Danilo Oliveira <danilooliveira4586@gmail.com>
2026-07-07 18:31:20 -0300

    Adicionando dados faker no campo de texto

--- cypress/e2e/task.cy.js
+++ cypress/e2e/task.cy.js
@@ -1,15 +1,29 @@
 /// <reference types="cypress" />
 
-describe('Tarefas', ()=>{
+import { faker } from '@faker-js/faker';
 
-    it('Deve realizar o cadastro da tarefa com sucesso', ()=>{
-        //acessando o endereço do sistema
+describe('Tarefas', () => {
+
+    it('Deve realizar o cadastro da tarefa com sucesso', () => {
+        // Acessando o endereço do sistema
         cy.visit('http://localhost:3000/')
 
         // Adicionando um valor ao campo de texto atráves do atributo placeholder do elemento input
-        cy.get('input[placeholder="Add a new task"]')
+        cy.get('input[placeholder="Add a new Task"]')
             .type('Estudando cypress')
-        
+
+        // Clicando no botão de criar, utilizando a função contains do cypress
+        cy.contains('button', 'Create').click()
+    })
+
+    it.only('Deve realizar cadastro da tarefa com dados Faker', () => {
+        // Acessando o endereço do sistema
+        cy.visit('http://localhost:3000/')
+
+        // Adicionando um dado faker ao valor do campo de texto atráves do atributo placeholder do elemento input
+        cy.get('input[placeholder="Add a new Task"]')
+            .type('Estudar a música ' + faker.music.songName())
+
         // Clicando no botão de criar, utilizando a função contains do cypress
         cy.contains('button', 'Create').click()
     })
