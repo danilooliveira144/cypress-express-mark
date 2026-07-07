a008a209ee05d219502807a8cde4f011028b2f38
Danilo Oliveira <danilooliveira4586@gmail.com>
2026-07-07 19:09:51 -0300

    Deletando tarefa através do endpoit de deletar tarefa

--- cypress/e2e/task.cy.js
+++ cypress/e2e/task.cy.js
@@ -16,7 +16,7 @@ describe('Tarefas', () => {
         cy.contains('button', 'Create').click()
     })
 
-    it.only('Deve realizar cadastro da tarefa com dados Faker', () => {
+    it('Deve realizar cadastro da tarefa com dados Faker', () => {
         // Acessando o endereço do sistema
         cy.visit('http://localhost:3000/')
 
@@ -26,5 +26,31 @@ describe('Tarefas', () => {
 
         // Clicando no botão de criar, utilizando a função contains do cypress
         cy.contains('button', 'Create').click()
+    })
+
+    it.only('Deve realizar cadastro da tarefa com sucesso, consumindo a API Rest', () => {
+
+        /**
+         * Enviando um requisição http para o endpoint, utilizando o método 'DELETE',
+         * passando o valor de name no corpo da requsição e validando o retorno da resposta da API
+         */
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks/',
+            method: 'DELETE',
+            body: { name: 'Estudando cypress' }
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+
+        // Acessando o endereço do sistema
+        cy.visit('http://localhost:3000/')
+
+        // Adicionando um valor ao campo de texto atráves do atributo placeholder do elemento input
+        cy.get('input[placeholder="Add a new Task"]')
+            .type('Estudando cypress')
+
+        // Clicando no botão de criar, utilizando a função contains do cypress
+        cy.contains('button', 'Create').click()
+
     })
 })
